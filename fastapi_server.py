2850d51b9164ef2d1b9a6c993a593885569f929d
jae <gjwodud119@gmail.com>
2024-03-08 12:43:15 +0900

    Update: inference 함수 분리

--- fastapi_server.py
+++ fastapi_server.py
@@ -8,6 +8,18 @@ import onnxruntime as ort
 from time import time
 import base64
 
+def inference(image, session):
+    height, width = image.shape[:2]
+    if width != 640 and height == 640:
+        raise Exception("이미지 크기를 640x640으로 맞춰주세요.")
+    
+    image = image.astype(np.float32) / 255.0
+    image = np.transpose(image, (2, 0, 1))  # Change data layout from HWC to CHW
+    image = np.expand_dims(image, axis=0)  # Add batch dimension
+
+    input_name = session.get_inputs()[0].name
+    output_names = [o.name for o in session.get_outputs()]
+    return session.run(output_names, {input_name: image})
 
 app = FastAPI()
 
@@ -18,15 +30,7 @@ async def on_receive_video(websocket: WebSocket):
     while True:
         data = await websocket.receive_bytes()
         image = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
-        image = cv2.resize(image, (640, 640))
-        image = image.astype(np.float32) / 255.0
-        image = np.transpose(image, (2, 0, 1))
-        image = np.expand_dims(image, axis=0)
-
-        # 모델 추론
-        input_name = session.get_inputs()[0].name
-        output_names = [o.name for o in session.get_outputs()]
-        outputs = session.run(output_names, {input_name: image})
+        outputs = inference(image, session)
 
         class_ids = outputs[0][0]
         bbox = outputs[1][0]
@@ -46,7 +50,11 @@ async def on_receive_video(websocket: WebSocket):
         await websocket.send_json({"bboxes": metadata})
 
 if __name__ == "__main__":
-    session = ort.InferenceSession('./yolow-l.onnx', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+    print("서버를 켜고 있습니다...")
+    session = ort.InferenceSession('./models/yolo.onnx', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+    dummy_image = np.random.randint(0, 256, (640, 640, 3), dtype=np.uint8)
+    inference(dummy_image, session)
     app.add_websocket_route("/ws", on_receive_video)
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=30300, log_level="info", ws="auto", lifespan="on")  
+    uvicorn.run(app, host="0.0.0.0", port=30348, log_level="info", ws="auto", lifespan="on")
+    
